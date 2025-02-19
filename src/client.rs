caf3755a3358140a3e50f7e84a3c9a9f1bb56c78
Orex <122088118+adrena-orex@users.noreply.github.com>
2025-02-19 13:34:18 +0400

    v1.1.9 - Limit orders (#13)

--- src/client.rs
+++ src/client.rs
@@ -1,11 +1,9 @@
 use {
-    crate::{
-        process_stream_message::process_stream_message, update_indexes::update_indexed_custodies,
-    },
+    crate::{process_stream_message::process_stream_message, update_indexes::update_indexed_custodies},
     adrena_abi::{
         main_pool::USDC_CUSTODY_ID,
         types::{Cortex, Position},
-        Pool,
+        LimitOrderBook, Pool,
     },
     anchor_client::{solana_sdk::signer::keypair::read_keypair_file, Client, Cluster},
     backoff::{future::retry, ExponentialBackoff},
@@ -23,14 +21,11 @@ use {
     tonic::transport::channel::ClientTlsConfig,
     yellowstone_grpc_client::{GeyserGrpcClient, Interceptor},
     yellowstone_grpc_proto::{
-        geyser::{
-            SubscribeRequest, SubscribeRequestFilterAccountsFilter,
-            SubscribeRequestFilterAccountsFilterMemcmp,
-        },
+        geyser::{SubscribeRequest, SubscribeRequestFilterAccountsFilter, SubscribeRequestFilterAccountsFilterMemcmp},
         prelude::{
             subscribe_request_filter_accounts_filter::Filter as AccountsFilterDataOneof,
-            subscribe_request_filter_accounts_filter_memcmp::Data as AccountsFilterMemcmpOneof,
-            CommitmentLevel, SubscribeRequestFilterAccounts,
+            subscribe_request_filter_accounts_filter_memcmp::Data as AccountsFilterMemcmpOneof, CommitmentLevel,
+            SubscribeRequestFilterAccounts,
         },
     },
 };
@@ -39,6 +34,7 @@ type AccountFilterMap = HashMap<String, SubscribeRequestFilterAccounts>;
 
 type IndexedPositionsThreadSafe = Arc<RwLock<HashMap<Pubkey, adrena_abi::types::Position>>>;
 type IndexedCustodiesThreadSafe = Arc<RwLock<HashMap<Pubkey, adrena_abi::types::Custody>>>;
+type IndexedLimitOrderBooksThreadSafe = Arc<RwLock<HashMap<Pubkey, adrena_abi::types::LimitOrderBook>>>;
 type PriorityFeesThreadSafe = Arc<RwLock<PriorityFees>>;
 // https://solscan.io/account/rec5EKMGg6MxZYaMdyBfgwp4d5rB9T1VQH5pJv5LtFJ
 pub const PYTH_RECEIVER_PROGRAM: &str = "rec5EKMGg6MxZYaMdyBfgwp4d5rB9T1VQH5pJv5LtFJ";
@@ -62,6 +58,7 @@ pub const CLOSE_POSITION_SHORT_CU_LIMIT: u32 = 285_000;
 pub const CLEANUP_POSITION_CU_LIMIT: u32 = 60_000;
 pub const LIQUIDATE_LONG_CU_LIMIT: u32 = 355_000;
 pub const LIQUIDATE_SHORT_CU_LIMIT: u32 = 256_000;
+pub const EXECUTE_LIMIT_ORDER_LONG_CU_LIMIT: u32 = 600_000; // TODO: Adapt to a value closer to the truth
 
 #[derive(Debug, Clone, Copy, Default, clap::ValueEnum)]
 enum ArgsCommitment {
@@ -121,9 +118,14 @@ pub fn get_position_anchor_discriminator() -> Vec<u8> {
     utils::derive_discriminator("Position").to_vec()
 }
 
+pub fn get_limit_order_book_anchor_discriminator() -> Vec<u8> {
+    utils::derive_discriminator("LimitOrderBook").to_vec()
+}
+
 async fn generate_accounts_filter_map(
     indexed_custodies: &IndexedCustodiesThreadSafe,
     indexed_positions: &IndexedPositionsThreadSafe,
+    indexed_limit_order_books: &IndexedLimitOrderBooksThreadSafe,
 ) -> AccountFilterMap {
     // Retrieve the price update v2 pdas from the indexed custodies
     let trade_oracle_keys: Vec<String> = indexed_custodies
@@ -134,28 +136,25 @@ async fn generate_accounts_filter_map(
         .collect();
 
     // Retrieve the existing positions keys - they are monitored for close events
-    let existing_positions_keys: Vec<String> = indexed_positions
-        .read()
-        .await
-        .keys()
-        .map(|p| p.to_string())
-        .collect();
+    let existing_positions_keys: Vec<String> = indexed_positions.read().await.keys().map(|p| p.to_string()).collect();
+
+    // Retrieve the existing limit order book keys - they are monitored for close events
+    let existing_limit_order_books_keys: Vec<String> =
+        indexed_limit_order_books.read().await.keys().map(|p| p.to_string()).collect();
 
     // Create the accounts filter map (on all positions based on discriminator, and the above price update v2 pdas)
     let mut accounts_filter_map: AccountFilterMap = HashMap::new();
 
     // Positions (will catch new positions created and modified positions)
     let position_filter_discriminator = SubscribeRequestFilterAccountsFilter {
-        filter: Some(AccountsFilterDataOneof::Memcmp(
-            SubscribeRequestFilterAccountsFilterMemcmp {
-                offset: 0,
-                data: Some(AccountsFilterMemcmpOneof::Bytes(
-                    get_position_anchor_discriminator(),
-                )),
-            },
-        )),
+        filter: Some(AccountsFilterDataOneof::Memcmp(SubscribeRequestFilterAccountsFilterMemcmp {
+            offset: 0,
+            data: Some(AccountsFilterMemcmpOneof::Bytes(get_position_anchor_discriminator())),
+        })),
     };
+
     let position_owner = vec![adrena_abi::ID.to_string()];
+
     accounts_filter_map.insert(
         "positions_create_update".to_owned(),
         SubscribeRequestFilterAccounts {
@@ -166,16 +165,50 @@ async fn generate_accounts_filter_map(
         },
     );
 
-    // Existing positions - We monitor these to catch when they are closed
+    if !existing_positions_keys.is_empty() {
+        // Existing positions - We monitor these to catch when they are closed
+        accounts_filter_map.insert(
+            "positions_close".to_owned(),
+            SubscribeRequestFilterAccounts {
+                account: existing_positions_keys,
+                owner: vec![],
+                filters: vec![],
+                nonempty_txn_signature: None,
+                },
+            );
+    }
+
+    let limit_order_book_owner = vec![adrena_abi::ID.to_string()];
+
+    let limit_order_book_filter_discriminator = SubscribeRequestFilterAccountsFilter {
+        filter: Some(AccountsFilterDataOneof::Memcmp(SubscribeRequestFilterAccountsFilterMemcmp {
+            offset: 0,
+            data: Some(AccountsFilterMemcmpOneof::Bytes(get_limit_order_book_anchor_discriminator())),
+        })),
+    };
+
     accounts_filter_map.insert(
-        "positions_close".to_owned(),
+        "limit_order_books_create_update".to_owned(),
         SubscribeRequestFilterAccounts {
-            account: existing_positions_keys,
-            owner: vec![],
-            filters: vec![],
+            account: vec![],
+            owner: limit_order_book_owner,
+            filters: vec![limit_order_book_filter_discriminator],
             nonempty_txn_signature: None,
         },
     );
+
+    if !existing_limit_order_books_keys.is_empty() {
+        // Existing limit order books - We monitor these to catch when they are closed
+        accounts_filter_map.insert(
+            "limit_order_books_close".to_owned(),
+            SubscribeRequestFilterAccounts {
+                account: existing_limit_order_books_keys,
+                owner: vec![],
+                filters: vec![],
+                nonempty_txn_signature: None,
+                },
+            );
+    }
 
     // Price update v2 pdas - the price updates
     let price_feed_owner = vec![PYTH_RECEIVER_PROGRAM.to_owned()];
@@ -214,6 +247,7 @@ async fn main() -> anyhow::Result<()> {
     let indexed_positions: IndexedPositionsThreadSafe = Arc::new(RwLock::new(HashMap::new()));
     // The array of indexed custodies - These are not directly observed, but are needed for instructions and to keep track of which price update v2 accounts are observed
     let indexed_custodies: IndexedCustodiesThreadSafe = Arc::new(RwLock::new(HashMap::new()));
+    let indexed_limit_order_books: IndexedLimitOrderBooksThreadSafe = Arc::new(RwLock::new(HashMap::new()));
 
     // The default exponential backoff strategy intervals:
     // [500ms, 750ms, 1.125s, 1.6875s, 2.53125s, 3.796875s, 5.6953125s,
@@ -223,6 +257,7 @@ async fn main() -> anyhow::Result<()> {
         let zero_attempts = Arc::clone(&zero_attempts);
         let indexed_positions = Arc::clone(&indexed_positions);
         let indexed_custodies = Arc::clone(&indexed_custodies);
+        let indexed_limit_order_books = Arc::clone(&indexed_limit_order_books);
         let mut periodical_priority_fees_fetching_task: Option<JoinHandle<Result<(), backoff::Error<anyhow::Error>>>> = None;
 
         async move {
@@ -240,20 +275,12 @@ async fn main() -> anyhow::Result<()> {
             drop(zero_attempts);
 
             let commitment = args.get_commitment();
-            let mut grpc = args
-                .connect()
-                .await
-                .map_err(backoff::Error::transient)?;
+            let mut grpc = args.connect().await.map_err(backoff::Error::transient)?;
 
             let payer = read_keypair_file(args.payer_keypair.clone()).unwrap();
             let payer = Arc::new(payer);
-            let client = Client::new(
-                Cluster::Custom(args.endpoint.clone(), args.endpoint.clone()),
-                Arc::clone(&payer),
-            );
-            let program = client
-                .program(adrena_abi::ID)
-                .map_err(|e| backoff::Error::transient(e.into()))?;
+            let client = Client::new(Cluster::Custom(args.endpoint.clone(), args.endpoint.clone()), Arc::clone(&payer));
+            let program = client.program(adrena_abi::ID).map_err(|e| backoff::Error::transient(e.into()))?;
             log::info!("  <> gRPC, RPC clients connected!");
 
             // Fetched once
@@ -273,18 +300,12 @@ async fn main() -> anyhow::Result<()> {
                 .account::<adrena_abi::types::Custody>(USDC_CUSTODY_ID)
                 .await
                 .map_err(|e| backoff::Error::transient(e.into()))?;
-            indexed_custodies
-                .write()
-                .await
-                .insert(USDC_CUSTODY_ID, usdc_custody);
+            indexed_custodies.write().await.insert(USDC_CUSTODY_ID, usdc_custody);
 
             // ////////////////////////////////////////////////////////////////
             log::info!("1 - Retrieving and indexing existing positions and their custodies...");
             {
-                let position_pda_filter = RpcFilterType::Memcmp(Memcmp::new_base58_encoded(
-                    0,
-                    &get_position_anchor_discriminator(),
-                ));
+                let position_pda_filter = RpcFilterType::Memcmp(Memcmp::new_base58_encoded(0, &get_position_anchor_discriminator()));
                 let filters = vec![position_pda_filter];
                 let existing_positions_accounts = program
                     .accounts::<Position>(filters)
@@ -292,27 +313,36 @@ async fn main() -> anyhow::Result<()> {
                     .map_err(|e| backoff::Error::transient(e.into()))?;
                 // Extend the indexed positions map with the existing positions
                 indexed_positions.write().await.extend(existing_positions_accounts);
-                log::info!(
-                    "  <> # of existing positions parsed and loaded: {}",
-                    indexed_positions.read().await.len()
-                );
-
-                // Update the indexed custodies map based on the indexed positions
-                update_indexed_custodies(&program, &indexed_positions, &indexed_custodies).await?;
+                log::info!("  <> # of existing positions parsed and loaded: {}", indexed_positions.read().await.len());
             }
+
+            log::info!("2 - Retrieving and indexing existing limit order books...");
+            {
+                let limit_order_book_pda_filter = RpcFilterType::Memcmp(Memcmp::new_base58_encoded(0, &get_limit_order_book_anchor_discriminator()));
+                let filters = vec![limit_order_book_pda_filter];
+                let existing_limit_order_book_accounts = program
+                    .accounts::<LimitOrderBook>(filters)
+                    .await
+                    .map_err(|e| backoff::Error::transient(e.into()))?;
+                // Extend the indexed limit order books map with the existing limit order books
+                indexed_limit_order_books.write().await.extend(existing_limit_order_book_accounts);
+                log::info!("  <> # of existing limit order books parsed and loaded: {}", indexed_limit_order_books.read().await.len());
+            }
+
+            // Update the indexed custodies map based on the indexed positions
+            update_indexed_custodies(&program, &indexed_positions, &indexed_limit_order_books, &indexed_custodies).await?;
             // ////////////////////////////////////////////////////////////////
 
             // ////////////////////////////////////////////////////////////////
             // The account filter map is what is provided to the subscription request
             // to inform the server about the accounts we are interested in observing changes to
             // ////////////////////////////////////////////////////////////////
-            log::info!("2 - Generate subscription request and open stream...");
-            let accounts_filter_map =
-                generate_accounts_filter_map(&indexed_custodies, &indexed_positions).await;
+            log::info!("3 - Generate subscription request and open stream...");
+            let accounts_filter_map = generate_accounts_filter_map(&indexed_custodies, &indexed_positions, &indexed_limit_order_books).await;
             log::info!("  <> Account filter map initialized");
             let (mut subscribe_tx, mut stream) = {
                 let request = SubscribeRequest {
-                    ping: None,//Some(SubscribeRequestPing { id: 1 }),
+                    ping: None, //Some(SubscribeRequestPing { id: 1 }),
                     accounts: accounts_filter_map,
                     commitment: commitment.map(|c| c.into()),
                     ..Default::default()
@@ -326,7 +356,6 @@ async fn main() -> anyhow::Result<()> {
                 (subscribe_tx, stream)
             };
 
-
             // ////////////////////////////////////////////////////////////////
             // Side thread to fetch the median priority fee every 5 seconds
             // ////////////////////////////////////////////////////////////////
@@ -336,7 +365,7 @@ async fn main() -> anyhow::Result<()> {
                 ultra: 0,
             }));
             // Spawn a task to poll priority fees every 5 seconds
-            log::info!("3 - Spawn a task to poll priority fees every 5 seconds...");
+            log::info!("4 - Spawn a task to poll priority fees every 5 seconds...");
             #[allow(unused_assignments)]
             {
             periodical_priority_fees_fetching_task = Some({
@@ -366,11 +395,11 @@ async fn main() -> anyhow::Result<()> {
             // CORE LOOP
             //
             // Here we wait for new messages from the stream and process them
-            // if coming from the price update v2 accounts, we check for 
-            // liquidation/sl/tp conditions on the already indexed positions if 
+            // if coming from the price update v2 accounts, we check for
+            // liquidation/sl/tp conditions on the already indexed positions if
             // coming from the position accounts, we update the indexed positions map
             // ////////////////////////////////////////////////////////////////
-            log::info!("4 - Start core loop: processing gRPC stream...");
+            log::info!("5 - Start core loop: processing gRPC stream...");
             loop {
                 match timeout(Duration::from_secs(11), stream.next()).await {
                     Ok(Some(message)) => {
@@ -378,6 +407,7 @@ async fn main() -> anyhow::Result<()> {
                             message.map_err(|e| backoff::Error::transient(e.into())),
                             &indexed_positions,
                             &indexed_custodies,
+                            &indexed_limit_order_books,
                             &payer,
                             &args.endpoint.clone(),
                             &cortex,
@@ -403,7 +433,9 @@ async fn main() -> anyhow::Result<()> {
                         return Err(backoff::Error::transient(anyhow::anyhow!("Server Closed Connection")));
                     }
                     Err(_) => {
-                        log::warn!("No message received in 11 seconds, restarting connection (we should be getting at least a ping every 10 seconds)");
+                        log::warn!(
+                            "No message received in 11 seconds, restarting connection (we should be getting at least a ping every 10 seconds)"
+                        );
                         return Err(backoff::Error::transient(anyhow::anyhow!("Timeout")));
                     }
                 }
---
+++ src/handlers/execute_limit_order_short.rs
@@ -0,0 +1,100 @@
+use {
+    crate::{
+        handlers::create_execute_limit_order_short_ix, IndexedCustodiesThreadSafe, PriorityFeesThreadSafe,
+        EXECUTE_LIMIT_ORDER_LONG_CU_LIMIT,
+    },
+    adrena_abi::{LimitOrder, LimitOrderBook},
+    anchor_client::Program,
+    solana_client::rpc_config::RpcSendTransactionConfig,
+    solana_sdk::{compute_budget::ComputeBudgetInstruction, pubkey::Pubkey, signature::Keypair},
+    std::sync::Arc,
+};
+
+pub async fn execute_limit_order_short(
+    limit_order_book_key: &Pubkey,
+    limit_order_book: &LimitOrderBook,
+    limit_order: &LimitOrder,
+    indexed_custodies: &IndexedCustodiesThreadSafe,
+    program: &Program<Arc<Keypair>>,
+    priority_fees: &PriorityFeesThreadSafe,
+) -> Result<(), backoff::Error<anyhow::Error>> {
+    let indexed_custodies_read = indexed_custodies.read().await;
+    let custody = indexed_custodies_read.get(&limit_order.custody).unwrap();
+    let collateral_custody = indexed_custodies_read.get(&limit_order.collateral_custody).unwrap();
+
+    let pool_pda = adrena_abi::pda::get_pool_pda(&String::from("main-pool")).0;
+
+    let limit_order_book_pda = adrena_abi::pda::get_limit_order_book_pda(&pool_pda, &limit_order_book.owner).0;
+    let collateral_escrow_pda =
+        adrena_abi::pda::get_collateral_escrow_pda(&pool_pda, &limit_order_book.owner, &collateral_custody.mint).0;
+
+    let position_pda = adrena_abi::pda::get_position_pda(
+        &limit_order_book.owner,
+        &pool_pda,
+        &limit_order.custody,
+        limit_order.get_side(),
+    )
+    .0;
+
+    let transfer_authority_pda = adrena_abi::pda::get_transfer_authority_pda().0;
+
+    let (execute_limit_order_long_ix, execute_limit_order_long_ix_accounts) = create_execute_limit_order_short_ix(
+        &program.payer(),
+        &limit_order_book.owner,
+        &position_pda,
+        &pool_pda,
+        &transfer_authority_pda,
+        &collateral_escrow_pda,
+        &limit_order_book_pda,
+        &limit_order.custody,
+        &limit_order.collateral_custody,
+        custody,
+        collateral_custody,
+        limit_order.id,
+    );
+
+    let priority_fees_read = priority_fees.read().await;
+
+    let tx = program
+        .request()
+        .instruction(ComputeBudgetInstruction::set_compute_unit_price(priority_fees_read.high))
+        .instruction(ComputeBudgetInstruction::set_compute_unit_limit(
+            EXECUTE_LIMIT_ORDER_LONG_CU_LIMIT,
+        ))
+        .args(execute_limit_order_long_ix)
+        .accounts(execute_limit_order_long_ix_accounts)
+        .signed_transaction()
+        .await
+        .map_err(|e| {
+            log::error!("  <> Transaction generation failed with error: {:?}", e);
+            backoff::Error::transient(e.into())
+        })?;
+
+    let rpc_client = program.rpc();
+
+    let tx_hash = rpc_client
+        .send_transaction_with_config(
+            &tx,
+            RpcSendTransactionConfig {
+                skip_preflight: true,
+                max_retries: Some(0),
+                ..Default::default()
+            },
+        )
+        .await
+        .map_err(|e| {
+            log::error!("  <> Transaction sending failed with error: {:?}", e);
+            backoff::Error::transient(e.into())
+        })?;
+
+    log::info!(
+        "  <> Executed Limit Order Long {:#?} id {:?} - TX sent: {:#?}",
+        limit_order_book_key.to_string(),
+        limit_order.id,
+        tx_hash.to_string(),
+    );
+
+    // TODO wait for confirmation and retry if needed
+
+    Ok(())
+}
